ab7a2f08d8d7f9706393e997b473b79eac62790f
EC2 Default User <ec2-user@ip-172-31-33-74.us-east-2.compute.internal>
2025-10-22 22:43:01 +0000

    unused aws services

---
+++ scripts/deploy-ec2-instance/user_data_scripts/disable_unneccessary_services.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -sh
+
+sudo systemctl stop amazon-ssm-agent
+sudo systemctl disable amazon-ssm-agent
+#mask it to prevent it from being started again
+sudo systemctl mask amazon-ssm-agent
+
+sudo systemctl stop gssproxy
+sudo systemctl disable gssproxy
+
+#superceded by cron -why running on Amazon Linux by default?
+sudo systemctl stop atd
+sudo systemctl disable atd
