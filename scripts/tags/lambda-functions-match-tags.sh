68064a342ce405e7210885af64689f884ccb4a57
Teri Radichel <github@pwn.fyi>
2026-01-20 16:46:10 -0500

    Update lambda-functions-match-tags.sh

--- scripts/tags/lambda-functions-match-tags.sh
+++ scripts/tags/lambda-functions-match-tags.sh
@@ -1,7 +1,8 @@
 #!bin/bash 
 #Find all the functions matching a list of tags where ANY of the tags match (not ALL required)
+#AND a part fo the name (which you can set to nonsense if you only want to filter on tags - I use name for validation)
 TAG_FILTER="Tag1=Value1|Tag2=Value2|Tag3=Value3|Tag4=Value4"
-
+PROFILE="your profile name"
 NAME_FILTER="kawabunga"
 # ---------------------------------------
 
@@ -11,7 +12,7 @@ SEARCH_PATTERN=$(echo "$TAG_FILTER" | sed 's/=/": "/g' | sed 's/|/|" /g' | sed '
 
 while IFS=$'\t' read -r name arn; do
     # Fetch tags exactly like your original script
-    TAGS=$(aws lambda list-tags --resource "$arn" --profile pentester-role --region us-west-2)
+    TAGS=$(aws lambda list-tags --resource "$arn" --profile $PROFILE --region us-west-2)
 
     # CHECK: Name Match (Ignore Case) OR Tag Match
     if [[ "${name,,}" == *"${NAME_FILTER,,}"* ]] || echo "$TAGS" | grep -qE "$SEARCH_PATTERN"; then
