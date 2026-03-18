3c85ccc04f77d523e3ded047706668ef432c9945
Teri Radichel <github@pwn.fyi>
2026-03-18 12:35:29 -0400

    Create curl-json-ip-ranges.sh

---
+++ scripts/monitor-mac-aws-endpoints/curl-json-ip-ranges.sh
@@ -0,0 +1,5 @@
+#!/bin/bash.sh
+# use this script to updeate your json ip ranges file
+# could incorporate this into other script and ony update on certain intervals
+
+curl -O https://ip-ranges.amazonaws.com/ip-ranges.json
